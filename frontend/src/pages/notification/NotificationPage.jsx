403e569b6a48de15f65d8a8bca82ce65d62fdf05
Yasuhiro Hasegawa <yasuhiro.hasegawa@gmail.com>
2025-02-10 02:42:47 -0800

    3- notifications query and mutation added

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { toast } from "react-hot-toast";
+
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
+
+
 const NotificationPage = () => {
+	/* // below are for UI design. 
 	const isLoading = false;
 	const notifications = [
 		{
@@ -27,10 +34,58 @@ const NotificationPage = () => {
 			type: "like",
 		},
 	];
-
 	const deleteNotifications = () => {
 		alert("All notifications deleted");
 	};
+	*/
+
+	const queryClient = useQueryClient();
+
+	// Query for getting notifications
+	const {
+		data:notifications,
+		isLoading,
+	} = useQuery({
+		queryKey: ["notifications"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/notifications");
+				const data = await res.json();
+				if(!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
+
+	// Mutation for deleting notifications
+	const { 
+		mutate: deleteNotifications
+	} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch("/api/notifications", {
+					method: "DELETE",
+				});
+				const data = await res.json();
+				if(!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Notifications deleted successfully");
+			queryClient.invalidateQueries({ queryKey: ["notifications"] });
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
+
 
 	return (
 		<>
